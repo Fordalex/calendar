d4542934b906738e4a6573a70fe9339aaa687d28
Alex <alex96ford19@gmail.com>
2020-11-12 16:31:35 +0000

    Added jquery and the calendar js basic layout.

---
+++ assets/js/calendar.js
@@ -0,0 +1,68 @@
+var months = [
+    "January",
+    "Febuaray",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+var days = [
+    "Sun",
+    "Mon",
+    "Tues",
+    "Wed",
+    "Thur",
+    "Fri",
+    "Sat",
+]
+
+// Add days to a month container and append the the dateContainer.
+function createMonthCalendar(year, month) {
+    var monthDays = daysInMonth(year, month);
+    var calendarDays = [];
+    calendarDays.push('<div class="month-container">')
+    calendarDays.push(`<h2>${months[month]}</h2>`)
+
+    // Add the day titles at the top of the container.
+    for (let i = 0; i < days.length; i++) {
+        calendarDays.push(`<p class="day">${days[i]}</p>`)
+    }
+    // Add blank days to start the month off on the correct day.
+    var blankDays = new Date(year, month, 1).getDay();
+    for (let i = 0; i < blankDays; i++) {
+        calendarDays.push(`<div class="day-container blank-day-container">.</div>`);
+    }
+
+    // Add the total days in the month.
+    for (let i = 0; i < monthDays; i++) {
+        calendarDays.push(`<div class="day-container" id="date-${i+1}-${month + 1}-${year}">${i + 1}</div>`);
+    }
+
+    // Add the conatiner to the page.
+    calendarDays.push('</div>')
+    $('#dateContainer').append(calendarDays.join(""));
+}
+
+// Return the number of days in that month.
+function daysInMonth(year, month) {
+    return new Date(year, month + 1, 0).getDate();
+}
+
+// Append the month calenders to the page.
+for (let i = 0; i < 12; i++) {
+    createMonthCalendar(2020, i);
+}
+
+// Style set days/holidays.
+var today = new Date();
+var todayId = `#date-${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
+
+$(todayId).addClass('today');
+$('#date-25-12-2020').addClass('christmas');
--- index.php
+++ index.php
@@ -13,8 +13,18 @@
 
     <?php include_once 'templates/navigation.html' ?>
 
+    <div class="row m-0 p-0">
+        <div class="col-6">
+        <div id="dateContainer"></div>
+        </div>
+        <div class="col-6">
+            
+        </div>
+    </div>
+   
 
-    <script src="assets/js/javascript.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="assets/js/calendar.js"></script>
 </body>
 
 </html>
