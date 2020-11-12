f95c18f6bcef30bf176d853d15cce0f1ebf60936
Alex <alex96ford19@gmail.com>
2020-11-12 16:24:21 +0000

    Added bootstrap and started navigation.

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Calendar</title>
 </head>
 
 <body>
+
+    <?php include_once 'templates/navigation.html' ?>
 
 
     <script src="assets/js/javascript.js"></script>
