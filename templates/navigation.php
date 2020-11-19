b234331380b157a13c9437e68bbb445d9441206a
Alex <alex96ford19@gmail.com>
2020-11-19 11:34:56 +0000

    Edited the navigation bar to show different option to user that are loggin or not.

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,59 +1,66 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark pr-5">
-    <a class="navbar-brand text-light" href="index.php">Calendar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
-        <div></div>
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link text-light" href="index.php">Home</a>
-            </li>
-            <li class="nav-item dropdown" id="calendarNavLink">
+    <?php 
+        if (isset($_SESSION['username'])) {
+        echo '
+        <a class="navbar-brand text-light" href="index.php">Calendar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+            <div></div>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link text-light" href="index.php">Home</a>
+                </li>
+                <li class="nav-item dropdown" id="calendarNavLink">
+                    <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        View Calendar
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="view_daily.php">Daily</a>
+                        <a class="dropdown-item" href="view_monthly.php">Monthly</a>
+                        <a class="dropdown-item" href="view_yearly.php">Yearly</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown" id="addNavLink">
+                    <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Add
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="add_events.php">Events</a>
+                        <a class="dropdown-item" href="add_custom_chore.php">Chore</a>
+                        <a class="dropdown-item" href="add_category.php">Category</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown d-block d-lg-none">
                 <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    View Calendar
+                    ' . $_SESSION['username'] . '
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="view_daily.php">Daily</a>
-                    <a class="dropdown-item" href="view_monthly.php">Monthly</a>
-                    <a class="dropdown-item" href="view_yearly.php">Yearly</a>
+                    <a class="dropdown-item" href="profile.php">Profile</a>
+                    <hr class="my-0">
+                    <a class="dropdown-item" href="user_profile/logout.php">Logout</a>
                 </div>
             </li>
-            <li class="nav-item dropdown" id="addNavLink">
+
+            </ul>
+
+            <li class="nav-item dropdown d-none d-lg-block" id="userNavLink">
                 <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Add
+                    ' . $_SESSION['username'] . '
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="add_events.php">Events</a>
-                    <a class="dropdown-item" href="add_custom_chore.php">Chore</a>
-                    <a class="dropdown-item" href="add_category.php">Category</a>
+                    <a class="dropdown-item" href="profile.php">Profile</a>
+                    <hr class="my-0">
+                    <a class="dropdown-item" href="forms/start_guide.php">Get Set Up</a>
+                    <hr class="my-0">
+                    <a class="dropdown-item" href="user_profile/logout.php">Logout</a>
                 </div>
             </li>
-            <li class="nav-item dropdown d-block d-lg-none">
-            <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <?php echo $_SESSION['username'] ?>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="profile.php">Profile</a>
-                <hr class="my-0">
-                <a class="dropdown-item" href="user_profile/logout.php">Logout</a>
-            </div>
-        </li>
-
-        </ul>
-
-        <li class="nav-item dropdown d-none d-lg-block" id="userNavLink">
-            <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <?php echo $_SESSION['username'] ?>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="profile.php">Profile</a>
-                <hr class="my-0">
-                <a class="dropdown-item" href="forms/start_guide.php">Get Set Up</a>
-                <hr class="my-0">
-                <a class="dropdown-item" href="user_profile/logout.php">Logout</a>
-            </div>
-        </li>
-
+            ';
+            } else {
+                echo '<a class="navbar-brand text-light" >Calendar</a>';
+            }
+        ?>
     </div>
 </nav>
