529e44b33cd9f55f78f02748e3d15b2ef77821c9
Alex <alex96ford19@gmail.com>
2020-11-23 23:46:55 +0000

    Added more styling to the landing page and made the page form responsive.

--- index.php
+++ index.php
@@ -9,16 +9,29 @@ include_once 'templates/header.html';
 <body>
     <?php include_once 'templates/navigation.php'; ?>
 
-    <div class="row m-0 p-0 py-5 justify-content-center main-background">
-
-        <div class="col-12">
+    <header class="row m-0 p-0 py-5 justify-content-start main-background">
+        <div class="col-12 col-sm-9 col-md-7 col-lg-5 justify-content-center header-wrapper">
             <h1>Calendar</h1>
-            <p>Keep track on your chores and other cutsom activaties.</p>
-            <a target="_blank" href="/www.fordsdevelopment.co.uk" class="btn btn-dark">About Fordsdevelopment</a>
+            <p>Keep track on your chores and other custom activaties to help you become better than yesterday. View yearly and monthly progression insights, compare them to last month to push your limits.</p>
+            <a target="_blank" href="/www.fordsdevelopment.co.uk" class="btn btn-dark mr-4">About Fordsdevelopment</a>
             <a href="register.php" class="btn btn-light">Sign Up</a>
         </div>
+    </header>
 
-    </div>
+    <main class="row p-4 py-5 justify-content-center">
+        <div class="col-5">
+            <div class="box-container">
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates at voluptatum maxime pariatur soluta sint ex cupiditate eveniet dolor odio dolores laborum, impedit esse odit inventore rem voluptate numquam labore! Rem autem quo aperiam nulla! Enim, inventore eligendi? Nam, quas.</p>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="box-container">
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates at voluptatum maxime pariatur soluta sint ex cupiditate eveniet dolor odio dolores laborum, impedit esse odit inventore rem voluptate numquam labore! Rem autem quo aperiam nulla! Enim, inventore eligendi? Nam, quas.</p>
+            </div>
+        </div>
+    </main>
 
 
     <!-- end of page content -->
